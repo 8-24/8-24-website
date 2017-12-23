7f9ac74c9d944493decdd208c22a77e177b41089
= <kurtic.meh@gmail.com>
2017-12-23 12:40:51 +0100

    dashboard routes admin middleware

--- routes/web.php
+++ routes/web.php
@@ -31,46 +31,49 @@ Route::get('/works/{slug}', 'WorksController@ServItemIndex');
 Route::get('/labs', 'LabsCategoriesController@servIndex');
 //Route::get('/labs', )
 
+Route::group(['prefix' => '/admin',  'middleware' => 'auth'], function()
+{
+        /* Home */
+        Route::get('/', 'AdminController@index');
+        Route::get('/home', 'HomeController@adminIndex');
+        Route::get('/home/edit/{id}', 'HomeController@show');
+        Route::post('updateHomeSection', ['as' => 'updateHomeSection', 'uses' => 'HomeController@update']);
+        Route::post('addHomeSection', ['as' => 'addHomeSection', 'uses' => 'HomeController@store']);
+        Route::post('deleteHomeSection', ['as' => 'deleteHomeSection', 'uses' => 'HomeController@destroy']);
+        /*BLOG*/
+        Route::get('/blog', 'BlogPostController@adminIndex');
+        Route::get('/blog/edit/{id}', 'BlogPostController@adminShow');
+        Route::get('/blog/add', 'BlogPostController@adminAdd');
+        Route::post('updateBlogPost',  ['as' => 'updateBlogPost',  'uses' => 'BlogPostController@update']);
+        Route::post('addBlogPost', ['as' => 'addBlogPost',  'uses' => 'BlogPostController@store']);
+        /*Works*/
+        Route::get('/works', 'WorksController@adminIndex');
+        Route::get('/works/edit/{id}', 'WorksController@adminShow');
+        Route::get('/works/add', 'WorksController@adminAdd');
+        Route::post('updateWorks',  ['as' => 'updateWorks',  'uses' => 'WorksController@update']);
+        Route::post('addWorks', ['as' => 'addWorks',  'uses' => 'WorksController@store']);
+        
+        /* SEO */
+        Route::get('/seo-default-pages', 'SeoController@index');
+        Route::get('/seo-default-pages/edit/{id}', 'SeoController@show');
+        Route::post('updateSeo', ['as' => 'updateSeo', 'uses' => 'SeoController@update']);
+        
+        /* LAB */
+        Route::get('/labs/categories', 'LabsCategoriesController@adminIndex');
+        Route::post('addLabsCategory', ['as' => 'addLabsCategory', 'uses' => 'LabsCategoriesController@store']);
+        Route::post('editLabsCategory', ['as' => 'editLabsCategory', 'uses' => 'LabsCategoriesController@update']);
+        Route::post('deleteLabsCategory', ['as' => 'deleteLabsCategory', 'uses' => 'LabsCategoriesController@destroy']);
+        Route::get('/labs/categories/edit/{id}', 'LabsCategoriesController@adminShow');
+        Route::get('/labs/posts', 'LabsPostController@adminIndex');
+        Route::post('addLabPost', ['as' => 'addLabPost', 'uses' => 'LabsPostController@store']);
+        
+        /* Contact message */
+        Route::get('/contact', 'ContactController@adminIndex');
+        Route::post('deleteContactMessage', ['as' => 'deleteContactMessage', 'uses' => 'ContactController@destroy']);
+});
 
-/* Home */
-Route::get('/admin/home', 'HomeController@adminIndex');
-Route::get('/admin/home/edit/{id}', 'HomeController@show');
-Route::post('updateHomeSection', ['as' => 'updateHomeSection', 'uses' => 'HomeController@update']);
-Route::post('addHomeSection', ['as' => 'addHomeSection', 'uses' => 'HomeController@store']);
-Route::post('deleteHomeSection', ['as' => 'deleteHomeSection', 'uses' => 'HomeController@destroy']);
-/*BLOG*/
-Route::get('/admin/blog', 'BlogPostController@adminIndex');
-Route::get('/admin/blog/edit/{id}', 'BlogPostController@adminShow');
-Route::get('/admin/blog/add', 'BlogPostController@adminAdd');
-Route::post('updateBlogPost',  ['as' => 'updateBlogPost',  'uses' => 'BlogPostController@update']);
-Route::post('addBlogPost', ['as' => 'addBlogPost',  'uses' => 'BlogPostController@store']);
 
-/*Works*/
-Route::get('/admin/works', 'WorksController@adminIndex');
-Route::get('/admin/works/edit/{id}', 'WorksController@adminShow');
-Route::get('/admin/works/add', 'WorksController@adminAdd');
-Route::post('updateWorks',  ['as' => 'updateWorks',  'uses' => 'WorksController@update']);
-Route::post('addWorks', ['as' => 'addWorks',  'uses' => 'WorksController@store']);
 
-/*Home*/
-//Route::get('/admin/home', '');
 
-Route::get('/admin', 'AdminController@index');
 
-/* SEO */
-Route::get('/admin/seo-default-pages', 'SeoController@index');
-Route::get('/admin/seo-default-pages/edit/{id}', 'SeoController@show');
-Route::post('updateSeo', ['as' => 'updateSeo', 'uses' => 'SeoController@update']);
 
-/* LAB */
-Route::get('/admin/labs/categories', 'LabsCategoriesController@adminIndex');
-Route::post('addLabsCategory', ['as' => 'addLabsCategory', 'uses' => 'LabsCategoriesController@store']);
-Route::post('editLabsCategory', ['as' => 'editLabsCategory', 'uses' => 'LabsCategoriesController@update']);
-Route::post('deleteLabsCategory', ['as' => 'deleteLabsCategory', 'uses' => 'LabsCategoriesController@destroy']);
-Route::get('/admin/labs/categories/edit/{id}', 'LabsCategoriesController@adminShow');
-Route::get('admin/labs/posts', 'LabsPostController@adminIndex');
-Route::post('addLabPost', ['as' => 'addLabPost', 'uses' => 'LabsPostController@store']);
-
-/* Contact message */
-Route::get('/admin/contact', 'ContactController@adminIndex');
-Route::post('deleteContactMessage', ['as' => 'deleteContactMessage', 'uses' => 'ContactController@destroy']);
