e2abe2934f03ad0a5ef471d89f88fc0ceba91ab2
narcisse28 <thm.oswald@gmail.com>
2018-02-02 14:29:16 +0100

    labsPost - commit

--- resources/assets/js/components/LabsPost.js
+++ resources/assets/js/components/LabsPost.js
@@ -1,79 +1,46 @@
 import React, { Component } from 'react';
+import Parser from 'html-react-parser';
 import {
   Link
 } from 'react-router-dom'
 import {NavBarLogic} from '../logic/NavLogic';
-import ProgressiveImage from 'react-progressive-bg-image';
-
-export default class LabsCategory extends Component{
+import Sketch from '../logic/Sketch';
+export default class LabsPost extends Component{
   constructor(props){
     super(props);
-    this.state = {
-        category: {},
-        posts: []
-    }
+    this.state = {post: {}}
   }
+
   componentDidMount(){
     NavBarLogic("black", "color");
     window.scrollTo(0, 0);
-    var slugCat = this.props.match.params.category;
-    axios.get('https://www.8-24.ch/api/labs/categories/'+slugCat)
+    var path = this.props.location.pathname;
+    axios.get('https://www.8-24.ch/api/labs/posts/'+this.props.match.params.slug)
       .then(response => {
-        this.setState({category: response.data})
+        this.setState({post: response.data});
       }).catch(error => {
-        this.props.history.push('/labs');
-      });
-    axios.get('https://www.8-24.ch/api/labs/'+slugCat+'/posts')
-      .then(response => {
-        this.setState({posts: response.data})
-      }).catch(error => {
-        this.props.history.push('/labs');
+        this.props.history.push('/labs/'+this.props.match.params.category);
       });
   }
-
-  PostsList(list){
-    var posts = list.map((item) => {
-        return <div className="three columns margin-top-fifty" key={item.id}>
-          <div className="lab-project-wrap work-link" >
-            <Link className="lab-project-link" to={`/labs/${this.props.match.params.category}/${item.slug}`} ><h3 id={item.slug} >{item.title}</h3></Link>
-            <Link to={`/labs/${this.props.match.params.category}/${item.slug}`} >
-              <ProgressiveImage
-                id={item.slug}
-                className="lab-project-cover"
-                src={item.cover}
-                placeholder={item.cover}
-                style={{
-                  height: 200,
-                  backgroundSize: 'contain',
-                  backgroundPosition: 'center top',
-                }}
-              />
-            </Link>
-          </div>
-        </div>
-      }
-    );
-    return posts;
-  }
-
+  
   render(){
     return(
-      <div className="margin-top-hundred">
-        <h1 className="color-white">
-          {this.state.category.title}
-        </h1>
-        <p className="color-white">
-            {this.state.category.description}
-        </p>
-        <div className="row">
-            {this.PostsList(this.state.posts)}
+      <div className="margin-top-hundred color-white">
+        <h1 className="color-white">{this.state.post.title}</h1>
+        <div id="gallery-content">
+          <br/>
+          <h1>Hello LabsPost</h1>
+          {Parser(String(this.state.post.content))}
         </div>
         <hr/>
-        <Link to="/labs">
+        <Link to={`/labs/${this.props.match.params.category}`}>
           <img src="/img/arrow_back_white.svg"  className="arrow-back" />
         </Link>
       </div>
-
     )
   }
 }
+
+
+
+
